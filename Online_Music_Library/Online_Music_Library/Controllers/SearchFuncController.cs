f27eb61c2e60d0a4e889e1ac4598c1f42fa447bf
SanzanaKarimLora <lorasanzana@gmail.com>
2019-04-04 21:38:17 +0600

    Search Option

---
+++ Online_Music_Library/Online_Music_Library/Controllers/SearchFuncController.cs
@@ -0,0 +1,20 @@
+﻿using Online_Music_Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Online_Music_Library.Controllers
+{
+  
+    public class SearchFuncController : Controller
+    {
+        Online_Music_LibraryEntities1 dbSearch = new Online_Music_LibraryEntities1();
+        // GET: SearchFunc
+        public ActionResult SearchOp(string searching)
+        {
+            return View(dbSearch.tblSongs.Where(x=>x.song_name.Contains(searching) || searching==null).ToList());
+        }
+    }
+}
